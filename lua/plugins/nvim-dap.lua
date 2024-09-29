b564db9e658b715d1c28a23ccc2e6e86416181b5
Hanjae Lee <jayhanjaelee@gmail.com>
2024-09-29 23:16:54 +0900

    It is updated.

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -41,7 +41,16 @@ return {
 			require("dapui").setup()
 
 			require("nvim-dap-virtual-text").setup({
-				display_callback = function() end,
+				-- enabled = true,
+				-- enabled_commands = true,
+				-- highlight_changed_variables = true,
+				-- highlight_new_as_changed = false,
+				-- show_stop_reason = true,
+				-- commented = false,
+				-- only_first_definition = true,
+				-- all_references = false,
+				-- clear_on_continue = false,
+				-- display_callback = function() end,
 			})
 
 			vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,25 +1,25 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-    configs.setup({
-      ensure_installed = { 
-        "php",
-        "phpdoc",
-        "c",
-        "lua",
-        "python",
-        "vim",
-        "vimdoc",
-        "query",
-        "javascript",
-        "html" 
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },  
-    })
-  end
+		configs.setup({
+			ensure_installed = {
+				"php",
+				"phpdoc",
+				"c",
+				"lua",
+				"python",
+				"vim",
+				"vimdoc",
+				"query",
+				"javascript",
+				"html",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
