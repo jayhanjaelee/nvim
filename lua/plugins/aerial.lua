1d244966fb25bdb10be2d85c431fa3060a6ad13f
Hanjae Lee <jayhanjaelee@gmail.com>
2024-10-02 15:46:08 +0900

    Add aerial for method outline.

---
+++ lua/plugins/aerial.lua
@@ -0,0 +1,65 @@
+return {
+  "stevearc/aerial.nvim",
+  opts = {},
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons"
+  },
+  config = function()
+    require('aerial').setup({
+      backends = { "treesitter", "lsp", "markdown", "asciidoc", "man" },
+      layout = {
+        -- These control the width of the aerial window.
+        -- They can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+        -- min_width and max_width can be a list of mixed types.
+        -- max_width = {40, 0.2} means "the lesser of 40 columns or 20% of total"
+        max_width = { 40, 0.2 },
+        width = nil,
+        min_width = 10,
+
+        -- key-value pairs of window-local options for aerial window (e.g. winhl)
+        win_opts = {},
+
+        -- Determines the default direction to open the aerial window. The 'prefer'
+        -- options will open the window in the other direction *if* there is a
+        -- different buffer in the way of the preferred direction
+        -- Enum: prefer_right, prefer_left, right, left, float
+        default_direction = "prefer_right",
+
+        -- Determines where the aerial window will be opened
+        --   edge   - open aerial at the far right/left of the editor
+        --   window - open aerial to the right/left of the current window
+        placement = "window",
+
+        -- When the symbols change, resize the aerial window (within min/max constraints) to fit
+        resize_to_content = true,
+
+        -- Preserve window size equality with (:help CTRL-W_=)
+        preserve_equality = false,
+      },
+
+      -- highlight
+      highlight_mode = "split_width",
+
+      -- Highlight the closest symbol if the cursor is not exactly on one.
+      highlight_closest = false,
+
+      -- Highlight the symbol in the source buffer when cursor is in the aerial win
+      highlight_on_hover = false,
+
+      -- When jumping to a symbol, highlight the line for this many ms.
+      -- Set to false to disable
+      highlight_on_jump = false,
+
+      -- Jump to symbol in source window when the cursor moves
+      autojump = false,
+
+      on_attach = function(bufnr)
+        -- Jump forwards/backwards with '{' and '}'
+        vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "<leader>o", "<cmd>AerialToggle!<CR>")
+      end,
+    })
+  end,
+}
