960ce53e7a7a663c1a6ecfc012fd37b16b70278a
Hanjae Lee <jayhanjaelee@gmail.com>
2024-10-02 13:16:08 +0900

    Add vim fugitive.

--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -1,8 +1,15 @@
 return {
-	"navarasu/onedark.nvim",
-	priority = 1000,
-	fazy = false,
-	config = function()
-		vim.cmd([[colorscheme onedark]])
-	end,
+  "navarasu/onedark.nvim",
+  priority = 1000,
+  fazy = false,
+  config = function()
+    -- vim.cmd([[colorscheme onedark]])
+    require('onedark').setup({
+      transparent = true,
+      lualine = {
+        transparent = true
+      },
+      require('onedark').load(),
+    })
+  end,
 }
