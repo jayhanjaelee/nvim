864c51fe255cd9968cde76b0da0487fffb8d8fa0
Hanjae Lee <jayhanjaelee@gmail.com>
2024-10-07 00:19:29 +0900

    Keep directory synced in Netrw.

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -1,2 +1,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+-- keep the current directory and the browsing directory synced.
+-- this helps avoid the "move files" error.
+vim.g.netrw_keepdir = 0
