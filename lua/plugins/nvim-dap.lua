6529d97d4749222c86abc77cb4065b631a356691
Hanjae Lee <jayhanjaelee@gmail.com>
2024-10-08 16:01:42 +0900

    add keybindgs for dap.

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -12,9 +12,9 @@ return {
       local ui = require("dapui")
 
       -- windows
-      -- local phpDapSrc = os.getenv("USERPROFILE") .. "/vscode-php-debug/out/phpDebug.js"
+      local phpDapSrc = os.getenv("USERPROFILE") .. "/vscode-php-debug/out/phpDebug.js"
       -- unix
-      local phpDapSrc = os.getenv("PATH") .. "/vscode-php-debug/out/phpDebug.js"
+      -- local phpDapSrc = os.getenv("PATH") .. "/vscode-php-debug/out/phpDebug.js"
       -- php
       dap.adapters.php = {
         type = "executable",
@@ -59,18 +59,22 @@ return {
 
       vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
       vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+      vim.keymap.set("n", "<leader>c", ":lua require'dap'.clear_breakpoints()<cr>", { silent = true })
 
       -- Eval var under cursor
       vim.keymap.set("n", "<space>?", function()
         require("dapui").eval(nil, { enter = true })
       end)
 
+      vim.keymap.set("n", "<leader>do", ":lua require'dapui'.open()<cr>", { silent = true })
+      vim.keymap.set("n", "<leader>dt", ":lua require'dapui'.toggle()<cr>", { silent = true })
+      vim.keymap.set("n", "<leader>dc", ":lua require'dapui'.close()<cr>", { silent = true })
       vim.keymap.set("n", "<F1>", dap.continue)
       vim.keymap.set("n", "<F2>", dap.step_into)
       vim.keymap.set("n", "<F3>", dap.step_over)
       vim.keymap.set("n", "<F4>", dap.step_out)
       vim.keymap.set("n", "<F5>", dap.step_back)
-      vim.keymap.set("n", "<F13>", dap.restart)
+      vim.keymap.set("n", "<F12>", dap.restart)
 
       dap.listeners.before.attach.dapui_config = function()
         ui.open()
