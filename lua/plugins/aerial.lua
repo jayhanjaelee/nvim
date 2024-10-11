acc78ec67ba231bdfdc6dfa1533d0b872fe1f89f
Hanjae Lee <jayhanjaelee@gmail.com>
2024-10-12 00:16:19 +0900

    change width

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -13,9 +13,9 @@ return {
         -- They can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
         -- min_width and max_width can be a list of mixed types.
         -- max_width = {40, 0.2} means "the lesser of 40 columns or 20% of total"
-        max_width = { 40, 0.2 },
-        width = nil,
-        min_width = 10,
+        -- max_width = { 40, 0.2 },
+        width = 30,
+        -- min_width = 20,
 
         -- key-value pairs of window-local options for aerial window (e.g. winhl)
         win_opts = {},
