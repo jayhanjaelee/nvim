9f97801d9fa66ad8056531ad89cdb27548da55b6
Hanjae Lee <jayhanjaelee@gmail.com>
2024-10-14 20:22:54 +0900

    disable hover.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,20 +1,20 @@
 local mapKey = require("utils.keyMapper").mapKey
 
 -- hover
-vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
-  vim.lsp.handlers.hover,
-  {border = 'rounded'}
-)
-vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
-  vim.lsp.handlers.signature_help,
-  {border = 'rounded'}
-)
-
-vim.diagnostic.config({
-  float = {
-    border = 'rounded',
-  },
-})
+-- vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+--   vim.lsp.handlers.hover,
+--   {border = 'rounded'}
+-- )
+-- vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+--   vim.lsp.handlers.signature_help,
+--   {border = 'rounded'}
+-- )
+--
+-- vim.diagnostic.config({
+--   float = {
+--     border = 'rounded',
+--   },
+-- })
 
 -- Add border to the diagnostic popup window
 vim.diagnostic.config({
