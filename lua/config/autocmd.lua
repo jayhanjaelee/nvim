8a4882230355b161e52bfb13a7641eb4e152c16d
Hanjae Lee <jayhanjaelee@gmail.com>
2024-10-15 20:41:25 +0900

    Add large file handler.

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -19,3 +19,35 @@ vim.cmd([[autocmd FileType php setlocal shiftwidth=4 tabstop=4 softtabstop=4 exp
 vim.cmd([[autocmd FileType javascript setlocal shiftwidth=4 tabstop=4 softtabstop=4 expandtab]])
 vim.cmd([[autocmd FileType html setlocal shiftwidth=4 tabstop=4 softtabstop=4 expandtab]])
 vim.cmd([[autocmd FileType mustache setlocal shiftwidth=4 tabstop=4 softtabstop=4 expandtab]])
+
+-- large file loading performance
+local function disable_syntax_treesitter()
+    print("Big file, disabling syntax, treesitter and folding")
+    if vim.fn.exists(':TSBufDisable') == 2 then
+        vim.cmd('TSBufDisable autotag')
+        vim.cmd('TSBufDisable highlight')
+        -- add more treesitter-related disables here if needed
+    end
+
+    vim.o.foldmethod = 'manual'
+    vim.cmd('syntax clear')
+    vim.cmd('syntax off')  -- 'syntax off' should be used to turn off syntax highlighting
+    vim.cmd('filetype off')
+    vim.o.undofile = false
+    vim.o.swapfile = false
+    vim.o.loadplugins = false
+end
+
+local function check_file_size()
+    local max_size = 512 * 1024 -- 512 KB
+    local file = vim.fn.expand("%")
+    if vim.fn.getfsize(file) > max_size then
+        disable_syntax_treesitter()
+    end
+end
+
+vim.api.nvim_create_augroup("BigFileDisable", { clear = true })
+vim.api.nvim_create_autocmd({ "BufReadPre", "FileReadPre" }, {
+    group = "BigFileDisable",
+    callback = check_file_size,
+})
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -7,6 +7,12 @@ return {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
+        highlight = {
+          enable = true,
+          -- disable = function(lang, bufnr)
+          --   return vim.api.nvim_buf_line_count(bufnr) > 5000
+          -- end,
+        },
         ensure_installed = {
           "php",
           "phpdoc",
