12cb1634ddc94f69e56c374d50137c454a80e9c6
Hanjae Lee <jayhanjaelee@gmail.com>
2024-10-28 13:05:09 +0900

    Add aerial extension.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -51,9 +51,26 @@ return {
         extensions = {
           recent_files = {
             ignore_patterns = {}
-          }
+          },
+          aerial = {
+            -- Set the width of the first two columns (the second
+            -- is relevant only when show_columns is set to 'both')
+            col1_width = 4,
+            col2_width = 30,
+            -- How to format the symbols
+            format_symbol = function(symbol_path, filetype)
+              if filetype == "json" or filetype == "yaml" then
+                return table.concat(symbol_path, ".")
+              else
+                return symbol_path[#symbol_path]
+              end
+            end,
+            -- Available modes: symbols, lines, both
+            show_columns = "both",
+          },
         }
       }
+      require("telescope").load_extension("aerial")
 
       local opts = {
         layout_config = { height = 0.5 }
@@ -73,6 +90,10 @@ return {
 
       mapKey('<Leader>fh', function()
         builtin.help_tags(themes.get_ivy(opts))
+      end)
+
+      mapKey('<Leader>fo', function()
+        require("telescope").extensions.aerial.aerial(themes.get_ivy(opts))
       end)
 
       mapKey('gr', function()
