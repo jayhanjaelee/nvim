cd9bd58cbd5dc486eeb328dc15c37d8aa1d61487
Hanjae Lee <jayhanjaelee@gmail.com>
2024-11-01 15:30:41 +0900

    Change aerial window position.

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -58,8 +58,19 @@ return {
         -- Jump forwards/backwards with '{' and '}'
         -- vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
         -- vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-        vim.keymap.set("n", "<leader>o", "<cmd>AerialToggle!<CR>")
+        vim.keymap.set("n", "<leader>o", "<cmd>AerialToggle! right<CR>")
       end,
+      filter_kind = {
+        "Class",
+        "Constructor",
+        "Enum",
+        "Function",
+        "Interface",
+        "Module",
+        "Method",
+        "Struct",
+        "Property"
+      },
     })
   end,
 }
