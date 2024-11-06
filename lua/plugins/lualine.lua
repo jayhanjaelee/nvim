aa6601dc6d41a9c6264c138fa68509ea56b49f0b
Hanjae Lee <jayhanjaelee@gmail.com>
2024-11-06 15:02:53 +0900

    Change depth limit.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -45,7 +45,7 @@ return {
         },
         highlight = true,
         separator = " > ",
-        depth_limit = 0,
+        depth_limit = 3,
         depth_limit_indicator = "..",
         safe_output = true,
         lazy_update_context = false,
