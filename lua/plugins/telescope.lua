996cc4ad66f1373bb81db55f191820baaa4a5ed9
Hanjae Lee <jayhanjaelee@gmail.com>
2026-02-27 22:44:02 +0900

    Enable codeLens in Coc.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -30,6 +30,15 @@ return {
       require('telescope').setup{
         defaults = {
           layout_config = { height = 0.5 },
+          file_ignore_patterns = {
+            'node_modules',
+            'vendor/*',
+            '.*min.js',
+            '%.o$',
+            '%.d$',
+            '^bin/'
+            '*.log'
+          },
           mappings = {
             i = {
               ['<esc>'] = actions.close,
@@ -134,21 +143,9 @@ return {
     'nvim-telescope/telescope-ui-select.nvim',
     config = function()
       require('telescope').setup({
-        defaults = {
-          file_ignore_patterns = {
-            'node_modules',
-            'vendor/*',
-            '.*min.js',
-            '%.o$',
-            '%.d$',
-            '^bin/'
-            -- '.*.log'
-          },
-        },
         extensions = {
           ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            }
+            require("telescope.themes").get_dropdown {}
           }
         }
       })
