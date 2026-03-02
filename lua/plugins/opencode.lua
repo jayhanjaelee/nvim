c63dcb21b8066d67e7f20733bb108f7adcd125ec
Hanjae Lee <jayhanjaelee@gmail.com>
2026-03-02 16:07:28 +0900

    이미 opencode process 가 있는경우 terminal 세션 종료하도록 설정.

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -25,9 +25,47 @@ return {
     },
   },
   config = function()
+    local opencode_cmd = 'opencode --port'
+    ---@type snacks.terminal.Opts
+    local snacks_terminal_opts = {
+      win = {
+        position = 'right',
+        enter = false,
+        on_win = function(win)
+          require('opencode.terminal').setup(win.win)
+        end,
+      },
+    }
+
     ---@type opencode.Opts
     vim.g.opencode_opts = {
-      -- Your configuration, if any; goto definition on the type or field for details
+      server = {
+        start = function()
+          require('snacks.terminal').open(opencode_cmd, snacks_terminal_opts)
+        end,
+        -- 이미 term 세션이 있으면 종료하도록 설정. (toggle 시 process 종료안되는 문제)
+        stop = function()
+          local term = require('snacks.terminal').get(opencode_cmd, snacks_terminal_opts)
+          if term then
+            term:close()
+            if term.buf and vim.api.nvim_buf_is_valid(term.buf) then
+              vim.api.nvim_buf_delete(term.buf, { force = true })
+            end
+          end
+        end,
+        toggle = function()
+          local Snacks = require('snacks.terminal')
+          local term = Snacks.get(opencode_cmd, snacks_terminal_opts)
+          if term and term:valid() then
+            term:close()
+            if term.buf and vim.api.nvim_buf_is_valid(term.buf) then
+              vim.api.nvim_buf_delete(term.buf, { force = true })
+            end
+          else
+            Snacks.open(opencode_cmd, snacks_terminal_opts)
+          end
+        end,
+      },
     }
 
     vim.o.autoread = true -- Required for `opts.events.reload`
