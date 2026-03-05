0fb9c264f4ba140e34e048cd2574ee518e2b8513
Hanjae Lee <jayhanjaelee@gmail.com>
2026-03-05 21:31:31 +0900

    Add javascript and typescript LSP setting.

---
+++ lsp/ts_ls.lua
@@ -0,0 +1,13 @@
+-- ~/.config/nvim/lsp/ts_ls.lua
+return {
+  cmd = { 'typescript-language-server', '--stdio' },
+  filetypes = {
+    'javascript',
+    'javascriptreact',
+    'javascript.jsx',
+    'typescript',
+    'typescriptreact',
+    'typescript.tsx',
+  },
+  root_markers = { 'tsconfig.json', 'jsconfig.json', 'package.json', '.git' },
+}
