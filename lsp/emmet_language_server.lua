666e7c8498cc5ff1d41925b053128351cd472a43
Hanjae Lee <jayhanjaelee@gmail.com>
2026-03-07 03:41:12 +0900

    Add emmet

---
+++ lsp/emmet_language_server.lua
@@ -0,0 +1,13 @@
+return {
+  cmd = { "emmet-language-server", "--stdio" },
+  filetypes = {
+    "html",
+    "css",
+    "scss",
+    "less",
+    "javascriptreact",
+    "typescriptreact",
+    "vue",
+    "svelte",
+  },
+}
