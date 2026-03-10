4fd697d755b2469983f23df7841524f64bddc64a
Hanjae Lee <jayhanjaelee@gmail.com>
2026-03-10 20:46:39 +0900

    Add Signify keymap.

--- lua/plugins/vim-signify.lua
+++ lua/plugins/vim-signify.lua
@@ -1,6 +1,16 @@
 return {
   'mhinz/vim-signify',
+  desc = 'Git change indicator showing modifications in the sign column',
+  event = 'BufReadPost',
   config = function()
+    -- Update time for sign refresh
     vim.opt.updatetime = 100
+
+    -- View diff of current file
+    vim.keymap.set('n', '<leader>gd', ':SignifyDiff<cr>', { noremap = true, desc = 'SignifyDiff' })
+    -- View diff of current hunk
+    vim.keymap.set('n', '<leader>gp', ':SignifyHunkDiff<cr>', { noremap = true, desc = 'SignifyHunkDiff' })
+    -- Undo changes in current hunk
+    vim.keymap.set('n', '<leader>gu', ':SignifyHunkUndo<cr>', { noremap = true, desc = 'SignifyHunkUndo' })
   end
 }
