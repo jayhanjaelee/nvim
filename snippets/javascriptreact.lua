bd594f4d1d37472801b6ddebf3c13f4e749418e9
Hanjae Lee <jayhanjaelee@gmail.com>
2026-03-24 18:06:05 +0900

    Add snippets.

--- snippets/javascriptreact.lua
+++ snippets/javascriptreact.lua
@@ -5,6 +5,70 @@ local t = ls.text_node
 local i = ls.insert_node
 
 return {
+  -- AJAX 다운로드 버튼 핸들러
+  s('ajax_download', {
+    t({
+      "$(\".list button.",
+    }),
+    i(1, 'download-excel'),
+    t({
+      "\").click(function () {",
+      "\t\t\tlet year = $(this).attr('data-",
+    }),
+    i(2, 'year'),
+    t({
+      "');",
+      "\t\t\tlet semester_code = $(this).attr('data-",
+    }),
+    i(3, 'semester_code'),
+    t({
+      "');",
+      "",
+      "\t\t\tlet requestBody = {",
+      "\t\t\t\t'type': '",
+    }),
+    i(4, 'download_excel'),
+    t({
+      "',",
+      "\t\t\t\t'returnurl': '<?php echo $CFG->wwwroot . '",
+    }),
+    i(5, ""),
+    t({
+      "'; ?>',",
+      "\t\t\t\t'year': year,",
+      "\t\t\t\t'semester_code': semester_code,",
+      "\t\t\t};",
+      "",
+      "\t\t\t$.ajax({",
+      "\t\t\t\ttype: 'post',",
+      "\t\t\t\turl: '<?php echo $CFG->wwwroot . '",
+    }),
+    i(6, ""),
+    t({
+      "'; ?>',",
+      "\t\t\t\tdata: requestBody,",
+      "\t\t\t\tdataType: 'application/json',",
+      "\t\t\t\tsuccess: function(data) {",
+      "\t\t\t\t\t",
+    }),
+    i(7, '// do something'),
+    t({
+      "",
+      "\t\t\t\t},",
+      "\t\t\t\terror: function(error) {",
+      "\t\t\t\t\t",
+    }),
+    i(8, '// do something'),
+    t({
+      "",
+      "\t\t\t\t}",
+      "\t\t\t});",
+      "",
+      "\t\t\treturn false;",
+      "\t\t});",
+    }),
+  }),
+
   -- useRef
   s('useref', {
     t('const '), i(1, 'ref'), t(' = useRef('), i(2, 'null'), t(')'),
--- snippets/php.lua
+++ snippets/php.lua
@@ -5,6 +5,28 @@ local t = ls.text_node
 local i = ls.insert_node
 
 return {
+
+  s('mdl_get_course_format', {
+    t({'$course_format = course_get_format($id);', ''}),
+    t({'$course = $course_format->get_course();', ''}),
+  }),
+
+  s('mdl_get_course_context', {
+    t({'$course_context = context_course::instance($course->id);', ''})
+  }),
+
+  s('mdl_create_event', {
+    t({
+      "$params = array(",
+			"\t'context' => context_course::instance($courseid),",
+			"\t'objectid' => $week_add->id",
+		  '); ',
+      '$event = \\local_ubattendance\\event\\week_created::create($params);',
+      '$event->add_record_snapshot(\'local_ubattendance\', $week_add);',
+      '$event->trigger();'
+    })
+  }),
+
   -- measure execution time
   s('calc_time', {
     t({'$startTime = microtime(true);', ''}),
@@ -182,6 +204,70 @@ return {
     t({ '', '} else {', '  ' }),
     i(3),
     t({ '', '}' }),
+  }),
+
+  -- AJAX 다운로드 버튼 핸들러
+  s('ajax_download', {
+    t({
+      "$(\".list button.",
+    }),
+    i(1, 'download-excel'),
+    t({
+      "\").click(function () {",
+      "\t\t\tlet year = $(this).attr('data-",
+    }),
+    i(2, 'year'),
+    t({
+      "');",
+      "\t\t\tlet semester_code = $(this).attr('data-",
+    }),
+    i(3, 'semester_code'),
+    t({
+      "');",
+      "",
+      "\t\t\tlet requestBody = {",
+      "\t\t\t\t'type': '",
+    }),
+    i(4, 'download_excel'),
+    t({
+      "',",
+      "\t\t\t\t'returnurl': '<?php echo $CFG->wwwroot . '",
+    }),
+    i(5, ""),
+    t({
+      "'; ?>',",
+      "\t\t\t\t'year': year,",
+      "\t\t\t\t'semester_code': semester_code,",
+      "\t\t\t};",
+      "",
+      "\t\t\t$.ajax({",
+      "\t\t\t\ttype: 'post',",
+      "\t\t\t\turl: '<?php echo $CFG->wwwroot . '",
+    }),
+    i(6, ""),
+    t({
+      "'; ?>',",
+      "\t\t\t\tdata: requestBody,",
+      "\t\t\t\tdataType: 'application/json',",
+      "\t\t\t\tsuccess: function(data) {",
+      "\t\t\t\t\t",
+    }),
+    i(7, '// do something'),
+    t({
+      "",
+      "\t\t\t\t},",
+      "\t\t\t\terror: function(error) {",
+      "\t\t\t\t\t",
+    }),
+    i(8, '// do something'),
+    t({
+      "",
+      "\t\t\t\t}",
+      "\t\t\t});",
+      "",
+      "\t\t\treturn false;",
+      "\t\t});",
+    }),
   }),
 
 }
